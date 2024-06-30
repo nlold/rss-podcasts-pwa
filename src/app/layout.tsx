d9965038f9280f3ec7763b9e27b433df84665dc5
Nik Liubitskii <nlmozg@gmail.com>
2024-06-30 23:33:04 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,17 +12,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <head>
-        <title>Podcast RSS Listener</title>
-        <link rel="manifest" href="/manifest.json" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-        <link rel="apple-touch-icon" href="/icons/icon_x512.png" />
-        <meta name="theme-color" content="#333333" />
-        <meta
-          name="description"
-          content="Слушайте подкасты бесплатно без СМС и регистрации"
-        />
-      </head>
       <body>
         <div className="container">
           <div className="main">
